bb4d139959f96f74746a69d75105a8254520da19
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-16 16:54:17 -0500

    Added larger button preview when designing

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ presets =
 	{ 
 		"field" : "btn_rss", 
 		"data" : [
-			{ "k": "input", "v": "rss  feed       " }, 
+			{ "k": "input", "v": " rss  feed       " }, 
 			{ "k": "vertical", "v": "-1" }, 
 			{ "k": "color", "v": "fff" }, 
 			{ "k": "style", "v": "normal" }, 
 			{ "k": "stroke", "v": "" }, 
-			{ "k": "shadow", "v": "0.1" }, 
+			{ "k": "shadow", "v": "0.3" }, 
 			{ "k": "background", "v": "888" }, 
 			{ "k": "border", "v": "000" }, 
 			{ "k": "iborder", "v": "fff" }, 
@@ -25,17 +25,17 @@ presets =
 	}, { 
 		"field" : "btn_atom", 
 		"data" : [
-			{ "k": "input", "v": "atom  feed     " }, 
+			{ "k": "input", "v": " atom  feed     " }, 
 			{ "k": "vertical", "v": "-1" }, 
 			{ "k": "color", "v": "fff" }, 
 			{ "k": "style", "v": "normal" }, 
 			{ "k": "stroke", "v": "" }, 
-			{ "k": "shadow", "v": "0.1" }, 
+			{ "k": "shadow", "v": "0.3" }, 
 			{ "k": "background", "v": "888" }, 
 			{ "k": "border", "v": "000" }, 
 			{ "k": "iborder", "v": "fff" }, 
-			{ "k": "ladj", "v": "34" }, 
-			{ "k": "radj", "v": "34" }, 
+			{ "k": "ladj", "v": "33" }, 
+			{ "k": "radj", "v": "33" }, 
 			{ "k": "lseg", "v": "f73" }, 
 			{ "k": "rseg", "v": "888" }, 
 			{ "k": "lborder", "v": "fff" }, 
@@ -107,6 +107,7 @@ window.onload = () => {
 	w	= 88,
 	h	= 15,
 	c	= cr( 'canvas' ),
+	rc	= cr( 'canvas' ),
 	t	= gId( 'input' ),
 	v	= gId( 'vertical' ),
 	o	= gId( 'color' ),
@@ -128,15 +129,23 @@ window.onload = () => {
 	u	= gId( 'history' ),
 	g	= gId( 'background' ),
 	a	= gId( 'save' ),
-	x	= c.getContext( '2d' );
+	x	= c.getContext( '2d' ),
+	dx	= rc.getContext( '2d' );
 	
-	c.width		= w;
-	c.height	= h;
 	
 	gId( 'generated' ).appendChild( c );
-	x.imageSmoothingEnabled = false;
+	c.width = w * 2;
+	c.height = h * 2;
+	rc.width = w;
+	rc.height = h;
+	
+	dx.imageSmoothingEnabled	= false;
+	dx.shadowBlur			= 0;
+	
+	x.imageSmoothingEnabled 	= false;
 	x.textBaseline	= 'middle';
 	x.textAlign	= 'center';
+	x.shadowBlur	= 0;
 	
 	const r = () => {
 		b();
@@ -144,15 +153,13 @@ window.onload = () => {
 	};
 	
 	const b = () => {
-		const 
+		const
 		tx		= t.value,
 		cx		= c.width / 2,
 		cy		= ( c.height / 2 ) + parseInt( v.value ),
 		ts		= parseFloat( sh.value ),
-		lx		= parseInt( la.value ),
-		rx		= parseInt( ra.value );
-		
-		//console.log(f.value);
+		lx		= parseInt( la.value ) * 2,
+		rx		= parseInt( ra.value ) * 2;
 		
 		// Border
 		x.fillStyle	= '#' + d.value;
@@ -160,29 +167,29 @@ window.onload = () => {
 		
 		// Inner
 		x.fillStyle	= '#' + i.value;
-		x.fillRect( 1, 1, c.width - 2, c.height - 2 );
+		x.fillRect( 2, 2, c.width - 4, c.height - 4 );
 		
 		// Background
 		x.fillStyle	= '#' + g.value;
-		x.fillRect( 2, 2, c.width - 4, c.height - 4 );
+		x.fillRect( 4, 4, c.width - 8, c.height - 8 );
 		
 		// Left segment
 		x.fillStyle	= '#' + ls.value;
-		x.fillRect( 2, 2, lx, c.height - 4 );
+		x.fillRect( 4, 4, lx, c.height - 8 );
 		
 		// Left border
 		x.fillStyle	= '#' + lb.value;
-		x.fillRect( lx + 1, 2, 1, c.height - 4 );
+		x.fillRect( lx + 4, 4, 2, c.height - 8 );
 		
 		// Right segment
 		x.fillStyle	= '#' + rs.value;
-		x.fillRect( c.width - ( rx + 2 ), 2, rx, c.height - 4 );
+		x.fillRect( c.width - ( rx + 4 ), 4, rx, c.height - 8 );
 		
 		// Right border
 		x.fillStyle	= '#' + rb.value;
-		x.fillRect( c.width - ( rx + 3 ), 2, 1, c.height - 4 );
+		x.fillRect( c.width - ( rx + 5 ), 4, 2, c.height - 8 );
 		
-		x.font		= s.value + ' 9px Silkscreen';
+		x.font		= s.value + ' 19px Silkscreen';
 		
 		// Text stroke
 		if ( k.value ) {
@@ -190,20 +197,24 @@ window.onload = () => {
 			x.lineWidth	= 2;
 			x.strokeText( tx, cx, cy );
 		}
+		
 		// Foreground text
 		x.strokeStyle	= 'rgba( 0, 0, 0, ' + ts + ' )';
 		x.lineWidth	= 0;
 		x.strokeText( tx, cx, cy );
+		
 		x.fillStyle	= '#' + o.value;
 		x.fillText( tx, cx, cy );
 	};
 	
 	a.addEventListener( 'click',  function() {
-		const 
+		dx.drawImage( c, 0, 0, c.width, c.height, 0, 0, w-1, h-1 );
+		
+		const
 		el = cr( 'li' ),
 		im = cr( 'img' ),
 		ah = cr( 'a' ),
-		dt = c.toDataURL(),
+		dt = rc.toDataURL(),
 		dl = gk( dt.replace( /\W/g, '' ) );
 		
 		ah.setAttribute( 'download', dl );
